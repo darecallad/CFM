88a67ee6bf9812704cc956ab3c06ec076d0246f9
Chung An Chen <Ashane199080706@hotmail.com.tw>
2017-10-20 18:09:38 -0700

    test with database

---
+++ src/mainpages/main.java
@@ -0,0 +1,60 @@
+package mainpages;
+
+import java.io.IOException;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class main
+ */
+@WebServlet("/main")
+public class main extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public main() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+    
+    // SQL
+    public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+    	try
+        {
+            Class.forName( "com.mysql.jdbc.Driver" );
+        }
+        catch( ClassNotFoundException e )
+        {
+            throw new ServletException( e );
+        }
+		
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		
+		// connect with Index.jsp
+		request.getRequestDispatcher( "Index.jsp" ).forward( request, response );
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
